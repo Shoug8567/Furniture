1ced6266999cc9bd397f03a93002664f82294993
Shoug8567 <166750875+Shoug8567@users.noreply.github.com>
2024-04-11 21:46:53 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    })
+}
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    })
+}
+const navLink = document.querySelectorAll('.nav-link')
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const membersList = document.getElementById('members-list');
+
+    function Reorder_randomly() {
+        for (let i = membersList.children.length; i >= 0; i--) {
+            membersList.appendChild(membersList.children[Math.random() * i | 0]);
+        }
+    }
+
+    Reorder_randomly();
+
+});
